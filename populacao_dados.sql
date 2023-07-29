245e794b5aea1ac1af4b09c98c52b270e4d7b65d
YohanBZ <barbozayohan@gmail.com>
2023-07-29 16:14:03 -0300

    feat: :memo: Adição de registros na tabela autores

---
+++ populacao_dados.sql
@@ -0,0 +1,8 @@
+use biblioteca; 
+
+INSERT INTO `autores`(id, nome, data_nascimento, nacionalidade) VALUES
+(1, 'Jorginho', '1960-01-01', 'Brasileiro'),
+(2, 'Juldrene', '1970-02-02', 'Argentino'),
+(3, 'Eurizona', '1980-03-03', 'Uruguaio'),
+(4, 'Ostralha', '1990-04-04', 'Chileno'),
+(5, 'Xercilia', '2000-05-05', 'Paraguaio');
