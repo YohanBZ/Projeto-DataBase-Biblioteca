13e7d899934f89c0577bd47f8a3f42c029143727
YohanBZ <barbozayohan@gmail.com>
2023-07-29 16:29:50 -0300

    feat: :construction: Adição de registros na tabela endereços

--- populacao_dados.sql
+++ populacao_dados.sql
@@ -6,3 +6,10 @@ INSERT INTO `autores`(id, nome, data_nascimento, nacionalidade) VALUES
 (3, 'Eurizona', '1980-03-03', 'Uruguaio'),
 (4, 'Ostralha', '1990-04-04', 'Chileno'),
 (5, 'Xercilia', '2000-05-05', 'Paraguaio');
+
+INSERT INTO `enderecos`(id, rua, numero, cep, bairro, cidade, estado, pais) VALUES
+(1, 'Rua Borboletas Psicodélicas', 49, '78930-628', 'Psicodelico', 'Curitiba', 'PR', 'Brasil'),
+(2, 'Rua Travessa Sem Destino', 201, '78932-000', 'SemHistória', 'Curitba', 'PR', 'Brasil'),
+(3, 'Rua Invocação do Mal', 666, '72000-666', 'Maldestino', 'Curitba', 'PR', 'Brasil'),
+(4, 'Rua Jacinto Machado', 69, '69006-001', 'Pinopolis', 'Curitba', 'PR', 'Brasil'),
+(5, 'Rua Frutas Verdes', 500, '74457-030', 'arvaria', 'Curitba', 'PR', 'Brasil');
